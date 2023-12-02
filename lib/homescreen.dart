440be56cf0596b75136a3f411bad16055467ade9
Shashwat Rai <shashwatrai575@gmail.com>
2023-12-03 02:12:05 +0530

    first commit

---
+++ lib/homescreen.dart
@@ -0,0 +1,52 @@
+import 'package:blocpractice/blocs/internet_bloc.dart';
+import 'package:blocpractice/blocs/internet_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: BlocConsumer<InternetBloc, InternetState>(
+          listener: (context, state) {
+            if(state is InternetGainedState){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Internet Connected'),
+                backgroundColor: Colors.green,
+                ),
+              );
+            }
+            else if(state is InternetLostState){
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Internet Lost'),
+                backgroundColor: Colors.red,
+                ),
+              );
+            }
+          },
+          builder: (context, state) {
+            if(state is InternetGainedState){
+ return Text('Connected');
+            }
+            else if(state is InternetLostState){
+              return Text("Not Connected");
+            }
+            else{
+              return Text('Loading...');
+            }
+          },
+        ),
+//         child: BlocBuilder<InternetBloc, InternetState>(
+//           builder: (context, state) {
+//             
+
+//           },
+//         ),
+      ),
+    );
+  }
+}
